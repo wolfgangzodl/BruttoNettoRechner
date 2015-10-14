11589df02b900527cc5dcb674ef0384e3d83d66f
wolfgangzodl <zodlwolfgang@gmail.com>
2015-10-14 18:45:33 +0200

    onClickBerechnen implementiert
    ueber den Button wird nun die Funktion onClickBerechnen aufgerufen.

--- app/src/main/java/com/example/wolfi/bruttonettorechner/FormularActivity.java
+++ app/src/main/java/com/example/wolfi/bruttonettorechner/FormularActivity.java
@@ -1,11 +1,58 @@
 package com.example.wolfi.bruttonettorechner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class FormularActivity extends AppCompatActivity {
+
+    public static final String BETRAG_KEY = "betrag";
+    public static final String BETRAG_ART = "art";
+    public static final String UST_PROZENT = "ust";
+
+    public FormularActivity() {
+    }
+
+    public void onClickBerechnen(View button){
+        final EditText txtBetrag = (EditText) findViewById(R.id.edt_Betrag);
+        final String tmpBetrag = txtBetrag.getText().toString();
+        float betrag = 0.0f;
+        if (tmpBetrag.length() > 0){
+            betrag = Float.parseFloat(tmpBetrag);
+        }
+        boolean isNetto = true;
+        final RadioGroup rg = (RadioGroup) findViewById(R.id.rg_art);
+        switch(rg.getCheckedRadioButtonId()){
+            case R.id.rb_art_netto:
+                isNetto = true;
+                break;
+            case R.id.rb_art_brutto:
+                isNetto = false;
+                break;
+            default:
+
+        }
+
+        final Spinner spinner = (Spinner) findViewById(R.id.sp_umsatzsteuer);
+        final int pos = spinner.getSelectedItemPosition();
+
+        final int[] prozentwerte = getResources().getIntArray(R.array.ust_werte);
+        final int prozentwert = prozentwerte[pos];
+
+        final Intent intent = new Intent(this, ErgebnisActivity.class);
+
+        intent.putExtra(BETRAG_KEY, betrag);
+        intent.putExtra(BETRAG_ART, isNetto);
+        intent.putExtra(UST_PROZENT, prozentwert);
+
+        startActivities(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
